4b7ee0b1f2d662c000ee79daf67ded5b66b2ce0e
Damián Cabello <jdamiancabello@gmail.com>
2018-01-11 14:44:32 +0100

    Añado ejercicio de refactorización.

---
+++ Proyectos/Refactorizar1/Refactorizar1/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactorizar1
+{
+    public class Persona
+    {
+        private int _id;
+        private string _nombre;
+
+        public Boolean Participante(Proyecto p)
+        {
+            for(int i = 0; i < p.Participantes.Count; i++)
+            {
+                if(p.Participantes[i]._id == this._id)
+                    return true; 
+            }
+            return false;
+        }
+    }
+}
---
+++ Proyectos/Refactorizar1/Refactorizar1/Proyecto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Refactorizar1
+{
+    public class Proyecto
+    {
+        private string _nombre;
+
+        public List<Persona> Participantes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
